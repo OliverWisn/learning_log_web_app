f94f735f27a2de07b3a95f7b4db93b9f6e4a1dc4
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2023-05-08 15:54:24 +0200

    Activate the model
    Activated the model and fixed errors in the scripts.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Topic(models.Model):
     """The topic that the user is learning."""
-    text = models.Charfield(max_lenght=200)
+    text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
