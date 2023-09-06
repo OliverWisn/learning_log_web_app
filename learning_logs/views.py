d4489d64983249471fadcaa9bb05bec8ff0242f1
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2023-09-06 13:39:06 +0200

    Create links from the topics page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -15,6 +15,6 @@ def topics(request):
 def topic(request, topic_id):
 	"""Displays a single topic and all related contents."""
 	topic = Topic.objects.get(id=topic_id)
-	entries = topic.entry.set.order_by('-date_added')
+	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
